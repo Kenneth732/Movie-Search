4d2b86c205f389af0a53ccacc616857a83c79456
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-04 06:08:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Movie from './components/Movies';
+
+function App() {
+  return (
+    <div className="App">
+      <Movie />
+    </div>
+  );
+}
+
+export default App;
